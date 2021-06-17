7ffafd5db3a7dc7383c47b900c97059911af3f82
KODIA772 <86063867+KODIA772@users.noreply.github.com>
2021-06-17 14:30:24 +0000

    Create login.php

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+require('config.php');
+session_start();
+if (isset($_POST['username'])){
+  $username = stripslashes($_REQUEST['username']);
+  $username = mysqli_real_escape_string($conn, $username);
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($conn, $password);
+    $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
+  $result = mysqli_query($conn,$query) or die(mysql_error());
+  $rows = mysqli_num_rows($result);
+  if($rows==1){
+      $_SESSION['username'] = $username;
+      header("Location: badge.html");
+  }else{
+    $message = "L'identifiant ou le mot de passe est incorrect.";
+  }
+}
+?>
+<form class="box" action="" method="post" name="login">
+<h1 class="box-title">Connexion</h1>
+<input type="text" class="box-input" name="username" placeholder="ENTRE VOTRE IDENTIFIANT" required>
+<input type="password" class="box-input" name="password" placeholder="ENTREZ VOTRE MOT DE PASSE	" required maxlength="4">
+<input type="submit" value="Connexion " name="submit" class="box-button">
+<p class="box-register">Vous êtes nouveau ici? <a href="register.html">S'inscrire</a></p>
+<?php if (! empty($message)) { ?>
+    <p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>
